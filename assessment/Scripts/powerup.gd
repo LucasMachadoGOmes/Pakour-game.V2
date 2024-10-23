82accee5466235d53cc8416acdddd5ea67ac5a36
LucasMachadoGOmes <165840565+LucasMachadoGOmes@users.noreply.github.com>
2024-10-23 20:13:02 +1300

    Update powerup.gd
    updating comment

--- assessment/Scripts/powerup.gd
+++ assessment/Scripts/powerup.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var timer = $Timer
 
-#this is the detection system to find out when the player touches the coin.
+#this is the detection system to find out when the player touches the coin. meow
 func _on_body_entered(body):
 	#this is the part that changes the players size
 		if body.has_method('add_powerup'):
