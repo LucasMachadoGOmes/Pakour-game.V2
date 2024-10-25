649ec5de2e2e8b65c90eb1b847d0cbb30b95b3aa
Aarav <104804813+AaravBansal@users.noreply.github.com>
2024-10-25 13:59:30 +1300

    Update coin.gd

--- assessment/Scripts/coin.gd
+++ assessment/Scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @onready var game_manager: Node = %GameManager
 
+# making collision and showing what happens once the collision has occured. 
+
 func _on_body_entered(_body):
 	game_manager.add_point()
 	queue_free()
